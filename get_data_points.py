cef600514cbd2e957b2c155bba98e0979d7d0edf
Your Name <you@example.com>
2025-03-18 18:47:11 +0000

    Import external function

--- get_data_points.py
+++ get_data_points.py
@@ -1,7 +1,20 @@
 import pandas as pd
 import os
+import get_master_files as gf
+
+
+person_files = gf.concat_fars_ext("person.csv")
+person_files
 
 accident_file = pd.read_csv("/Users/sebastianfirrell/Desktop/DPR Data Analysis/Data Exports/Amanda Demanda - Drunk Driving Capitals Data/Master Accidents.csv")
 person_file = pd.read_csv("/Users/sebastianfirrell/Desktop/DPR Data Analysis/Data Exports/Amanda Demanda - Drunk Driving Capitals Data/Persons Master Sheet.csv")
 commuter_cities = pd.read_csv("/Users/sebastianfirrell/Desktop/DPR Data Analysis/Data Downloads/Combined Datasets/Commuter Vehicles by city.csv")
 
+# Get drunk driving fatalities per city
+# Get drunk driving fatalities as percentage of total fatalities
+# Get car most involved in drunk driving
+# Get time which DUIs are most likely to happen
+
+accident_file
+
+print(person_files)
