80d9210ff4d75eda63a0e978c4d5247ae8acd20c
Your Name <you@example.com>
2025-03-20 14:44:19 +0000

    Get DUI Deaths

--- get_master_files.py
+++ get_master_files.py
@@ -34,10 +34,6 @@ def concat_fars_ext(f): # Takes the file we want from FARS as sole argument
                 master_file_df.to_csv(f"{filepath_out}/{filename_out}.csv", index=False)
                 return (f"{filepath_out}/{filename_out}.csv")
             
-def merge_datasets(a, b):
-    pd.merge(a, b)
-
-
 
 if __name__ == "__main__":
     main()
