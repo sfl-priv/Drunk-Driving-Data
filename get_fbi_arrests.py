21272f5f56b9104ad49e1645c76c1b013719e4c9
Your Name <you@example.com>
2025-03-21 18:58:55 +0000

    Get FBI Information

--- get_fbi_arrests.py
+++ get_fbi_arrests.py
@@ -2,3 +2,10 @@ import requests
 import pandas as pd
 
 key = "y1fxpEZRHEzCjNHbGrup3Y1wRQ6gf64grLBNb8P4"
+
+seed_list = pd.read_csv("/Users/sebastianfirrell/Desktop/DPR Data Analysis/Data Code/Amanda Demanda - Drunk Driving Hotspots Code/States and Cities.csv")
+
+
+print(seed_list)
+
+read_
