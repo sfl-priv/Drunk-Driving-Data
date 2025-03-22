3e5727d73d316028a31804ca63ad9296422e0905
Your Name <you@example.com>
2025-03-22 15:26:30 +0000

    Get FBI Information

--- get_fbi_arrests.py
+++ get_fbi_arrests.py
@@ -3,9 +3,24 @@ import pandas as pd
 
 key = "y1fxpEZRHEzCjNHbGrup3Y1wRQ6gf64grLBNb8P4"
 
+#Import list of all cities we want to check
 seed_list = pd.read_csv("/Users/sebastianfirrell/Desktop/DPR Data Analysis/Data Code/Amanda Demanda - Drunk Driving Hotspots Code/States and Cities.csv")
 
+base_url = "https://api.usa.gov/crime/fbi/cde"
+url = f"{base_url}/AK?API_KEY={key}"
 
-print(seed_list)
-
-read_
+#Check the endpoint connection is successful
+response = requests.get(url)
+if response == "200":
+    print("Request Successful")
+else:
+    print(f"Error: {response}")
+    
+# def get_seed_list_city():
+    
+# def get_fbi_info():
+    
+# def main():
+    
+if __name__ == "__main__":
+    main()
