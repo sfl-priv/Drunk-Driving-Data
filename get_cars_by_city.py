8672925e92844e8502c1421eb110cfc79f1c21d2
Your Name <you@example.com>
2025-03-28 16:27:09 +0000

    Common Cars Complete

--- get_cars_by_city.py
+++ get_cars_by_city.py
@@ -10,6 +10,7 @@ states_and_cities_csv = pd.read_csv("States and Cities.csv")
 cities_only = states_and_cities_csv['City']
 cities_only.tolist()
 
+# Get all unique states in a set
 states_only = states_and_cities_csv['State']
 states_only.tolist()
 unique_states = set(states_only)
@@ -20,7 +21,7 @@ def main():
     
     Find the cars that have been the most involved in drunk driving accidents for a list of cities.
     
-    Based on the datasets in the NHTSA FARS downloads
+    Based on the datasets in the NHTSA FARS downloads.
     
     '''
 
@@ -40,10 +41,9 @@ def main():
         'Occurence Count 2' : count_col_2,
     }
         
-    # Filter master file only to target states
+    # Filter master file only to target states where our cities are
     state_filtered_file = file[(file["STATENAME_x"].isin(unique_states))]
     
-        
     # Iterate over the list of cities we want the cars for
     for city in cities_only:
         one_city_df = state_filtered_file[state_filtered_file.CITYNAME == f'{city}'] # Filter Dataframe to target city
@@ -61,12 +61,12 @@ def main():
         count_col_2.append(most_common_count_2)
     
     # Convert dictionary to Dataframe object
-    final_target__df = pd.DataFrame(data=final_target_dict)
+    final_target_df = pd.DataFrame(data=final_target_dict)
     
-    # Choose location for final csv file
+    # Choose name and location for final spreadsheet
     filepath_out = input("File Save Location: ")
     filename_out = input("File Name: ")
-    final_target__df.to_csv(f"{filepath_out}/{filename_out}.csv")
+    final_target_df.to_csv(f"{filepath_out}/{filename_out}.csv")
 
 
 if __name__ == "__main__":
